f6652cc420ae055fd38bda81374c5433ccf0bc8f
OscarPV <oscarpv92@gmail.com>
2019-08-08 19:10:04 -0500

    Se agregan reducers para los 'api endpoints'.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
-//import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+
+import { 
+    SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_STREAM, 
+    FETCH_STREAMS, 
+    FETCH_STREAM, 
+    DELETE_STREAM, 
+    EDIT_STREAM
+} from './types';
 
 export const signIn = (userId) => {
     return {
@@ -20,3 +28,27 @@ export const createStream = formValues => async dispatch => {
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
 };
+
+export const fetchStreams = () => async dispatch => {
+    const response = await axios.get('http://localhost:3001/streams');
+
+    dispatch({ type:FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+    const response = await axios.get(`http://localhost:3001/streams/${id}`);
+
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await axios.put(`http://localhost:3001/streams${id}`, formValues);
+
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+    await axios.delete(`http://localhost:3001/${id}`);
+
+    dispatch({ type: DELETE_STREAM, payload: id });
+}
